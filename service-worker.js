427eb8d9c0cc4ea3828193862cb76848deaa7ed8
FireStorm <roger.angliss@gmail.com>
2026-08-24 08:11:47 +0100

    Bump v4.6.0 clean cache after repository cleanup

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kow-english-test-v4.6.0-clean-2';
+const CACHE='kow-english-test-v4.6.0-clean-3';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil(
